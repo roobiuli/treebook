90c62eaf99f56d99170495cbd261a3940abe5176
robert iulian <nicoara.robert@gmail.com>
2016-08-07 08:17:00 +0300

    Added devise

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  # attr_accessible :email, :password, :password_configrmation, :remember_me, :first_name, :last_name, :profile_name
+end
